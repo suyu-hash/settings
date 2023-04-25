970087da4859c0a6109c177f95d8b1b9cec97d09
suyu-hash <nabenabe11317@gmail.com>
2023-04-25 20:51:32 +0900

    add

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 sudo apt update
 
 ### fish and fisher ###
-chmod +x ./fish_setting/fish_setting.sh && ./fish_setting/fish_setting.sh
-chmod +x ./fish_setting/fish_settinger.fish && fish ./fish_setting/fish_settinger.fish
+sudo chmod +x ./fish_setting/fish_setting.sh && ./fish_setting/fish_setting.sh
+sudo chmod +x ./fish_setting/fish_settinger.fish && fish ./fish_setting/fish_settinger.fish
 
 # write my_config(fish) path
 echo "source $PWD/fish_setting/my_config.fish" >> ~/.config/fish/config.fish
