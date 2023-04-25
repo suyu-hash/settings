5c1d2a7c94194a2b3fa26ea5086e27ff955a7810
suyu-hash <nabenabe11317@gmail.com>
2023-04-25 16:35:14 +0900

    add imwheel

--- setup.sh
+++ setup.sh
@@ -9,6 +9,10 @@ chmod +x ./fish_setting/fish_settinger.fish && fish ./fish_setting/fish_settinge
 # write my_config(fish) path
 echo "source $PWD/fish_setting/my_config.fish" >> ~/.config/fish/config.fish
 
+### imwheel
+sudo apt install -y imwheel
+echo "imwheel -k -b \"4 5\"" >> ~/.bashrc
+
 ### fish
 # always write last line
 echo "exec fish" >> ~/.bashrc
