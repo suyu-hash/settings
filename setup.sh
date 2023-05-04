b106e365fcc202ec774dc00d59df089d3b1b516e
suyu-hash <nabenabe11317@gmail.com>
2023-05-04 23:04:10 +0900

    use /home/suyu/test/settings

--- setup.sh
+++ setup.sh
@@ -3,20 +3,20 @@
 sudo apt update
 
 ### fish and fisher ###
-sudo chmod +x ./fish_setting/setup_fish.sh && ./fish_setting/setup_fish.sh
-sudo chmod +x ./fish_setting/setup_fishger.fish && fish ./fish_setting/setup_fisher.fish
+sudo chmod +x $PWD/fish_setting/setup_fish.sh && $PWD/fish_setting/setup_fish.sh
+sudo chmod +x $PWD/fish_setting/setup_fisher.fish && fish $PWD/fish_setting/setup_fisher.fish
 
 # write my_config(fish) path
 echo "source $PWD/fish_setting/my_config.fish" >> ~/.config/fish/config.fish
 
 ### imwheel ###
 sudo apt install -y imwheel
-echo "imwheel -k -b \"4 5\"" >> ~/.bashrc
+#echo "imwheel -k -b \"4 5\"" >> ~/.bashrc # option
 
 
 ### ROS2 humble ###
-sudo chmod +x $PWD/ros2humble_setting/setup_ros2humble.sh && $PWD/ros2humble_setting/setup_ros2humble.sh
-echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+#sudo chmod +x $PWD/ros2humble_setting/setup_ros2humble.sh && $PWD/ros2humble_setting/setup_ros2humble.sh
+#echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
 
 
 ### fish ###
