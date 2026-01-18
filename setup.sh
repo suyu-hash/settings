65e78c81898f7e863a2553ea06ab5df5cc71eb46
sapphire <nabenabe1137@gmail.com>
2026-01-18 16:51:05 +0900

    fishをコメントアウト

--- setup.sh
+++ setup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-sudo apt update
+sudo apt update && sudo apt -y upgrade
 
 ### fish and fisher ###
 #sudo chmod +x $PWD/fish_setting/setup_fish.sh && $PWD/fish_setting/setup_fish.sh
 #sudo chmod +x $PWD/fish_setting/setup_fisher.fish && fish $PWD/fish_setting/setup_fisher.fish
 
 # write my_config(fish) path
-echo "source $PWD/fish_setting/my_config.fish" >> ~/.config/fish/config.fish
+#echo "source $PWD/fish_setting/my_config.fish" >> ~/.config/fish/config.fish
 
 ### imwheel ###
-sudo apt install -y imwheel
+#sudo apt install -y imwheel
 #echo "imwheel -k -b \"4 5\"" >> ~/.bashrc # option
 
 ### fish ###
 # to activate fish
 # always write last line
-echo "exec fish" >> ~/.bashrc
+#echo "exec fish" >> ~/.bashrc
 
 ### tmux ###
 sudo chmod +x $PWD/tmux/setup_tmux.sh && cd $PWD/tmux/ && ./setup_tmux.sh && cd ../
